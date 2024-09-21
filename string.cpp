6847d0c40fa876f8a7eed2745024a6e550bc633f
Ameya <125235218+ameypathak39@users.noreply.github.com>
2024-09-22 00:27:59 +0530

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,38 @@
+/*8/12/23
+#include<iostream>
+using namespace std;  // Add this line to include the std namespace
+
+int main() {
+    string firstName = "amey";
+    string lastName = "pathak";
+    string fullName = firstName + lastName;
+    cout << fullName;
+
+    return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+    string firstName = " babu";
+    string lastName = "nana";
+    string fullName  = firstName.append(lastName);
+    cout<< fullName;
+    return 0;
+}
+
+*/
+//Adding number string 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 20;
+    int y = 80;
+    int z = x + y;
+    
+    cout << "Value of z: " << z << endl; // Added << to display the value of z
+    return 0; 
+}
+
+
+
