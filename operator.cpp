b5504a58de9d9552df1062c7586ad1a4fd7bdc94
Ameya <125235218+ameypathak39@users.noreply.github.com>
2024-09-22 00:26:41 +0530

    Create operator.cpp

---
+++ operator.cpp
@@ -0,0 +1,22 @@
+/*
+7/12/23
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x = 100 + 50;
+    cout << "given value " << x; 
+    return 0;
+}
+
+************************************************
+8/12/23
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 100 + 20;
+    cout<<"given value" << x;
+    return 0;
+}
+*/
