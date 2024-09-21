61719c81027c841b725da40bd4fb4b672a00cb18
Ameya <125235218+ameypathak39@users.noreply.github.com>
2024-09-22 00:30:11 +0530

    Create user_input.cpp

---
+++ user_input.cpp
@@ -0,0 +1,40 @@
+/*7/12/23
+#include <iostream>
+
+using namespace std; // You need to specify std namespace for cout and cin
+
+int main() {
+    int x, y;
+    int sum;
+
+    cout << "Type a number: ";
+    cin >> x;
+
+    cout << "Type another number: ";
+    cin >> y;
+
+    sum = x + y;
+    cout << "The sum of the two numbers is: " << sum << endl; // Added endl for a newline
+
+    return 0; // Added a return statement
+}
+******************************************** 
+8/12/23
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    int sum;
+    cout<<"type first number";
+    cin>>x;
+    cout<<"type second number"; 
+    cin>>y;
+    sum=x+y;
+    cout<<"sum of two numbers is"<<sum<<endl;
+    return 0;
+
+}
+
+**************************************
+*/
+
