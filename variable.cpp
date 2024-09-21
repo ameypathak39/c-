9701d78faa32481ff8e97a319066d8d64e8ed95e
Ameya <125235218+ameypathak39@users.noreply.github.com>
2024-09-22 00:31:41 +0530

    Create variable.cpp

---
+++ variable.cpp
@@ -0,0 +1,21 @@
+ /*#include <iostream>
+
+   int myNum = 10;
+    myNum = 20;
+
+    std::cout << myNum;
+
+int main() {
+    int myAge = 20;
+    std::cout << "I am " << myAge << " years old ";
+    return 0;
+}
+*/
+
+
+#include<iostream>
+int main(){
+    int myAge = 30;
+    std:: cout << " I am "<< myAge <<"years ols";
+    return 0;
+}
