3fee54a376dd84575c739692ffbc0bdeea088604
Ameya <125235218+ameypathak39@users.noreply.github.com>
2024-09-22 00:30:53 +0530

    Create user_input_string.cpp

---
+++ user_input_string.cpp
@@ -0,0 +1,12 @@
+//10/12/23
+#include<iostream>
+using namespace std;
+int main(){
+    string firstName;
+    cout << "Write your first name ";
+    cin >> firstName;
+    cout << "Hello " << firstName << "!";
+    return 0;
+
+}
+
